010352f540a4081d1dd8c4080c283f61b51750db
ashubambal <ashutoshbambal97@gmail.com>
2025-11-25 17:20:55 +0530

    Update files

--- terraform/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "privateLink-s3" {
-  bucket = "ashutsoh-privatelink-infra-terraform-state-file"
-  region = "ap-south-1"
-  tags = {
-    Name        = "privatelink-infra-terraform-state-file"
-    Environment = "Dev"
-  }
-}
