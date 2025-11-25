2097fb8040f9ace3855dc47cb17ff127eb30da4b
ashubambal <ashutoshbambal97@gmail.com>
2025-11-25 12:29:10 +0530

    prod12-update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,12 +6,12 @@ variable "public_key" {
 
 variable "instance_type" {
   description = "Instance tpye used for EC2 instance"
-  type = string
-  default = "t3.micro"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "ami" {
   description = "ami id for EC2 instance"
-  type = string
-  default = "ami-02b8269d5e85954ef"
+  type        = string
+  default     = "ami-02b8269d5e85954ef"
 }
