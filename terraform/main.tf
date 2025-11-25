099aef6efca412bdc2f6290cb921a05379b7790c
ashubambal <ashutoshbambal97@gmail.com>
2025-11-25 16:29:59 +0530

    prod14-update

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,4 +115,11 @@ resource "aws_security_group" "private-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = var.tags
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0,0,0,0/0"]
+  }
 }
